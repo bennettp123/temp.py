331c5599e3791a063774802b1f69dbeed517d368
Bennett Perkins <benenttperkins@gmail.com>
2016-05-16 11:02:05 +0800

    journal_mode=WAL

--- db-setup.sh
+++ db-setup.sh
@@ -7,4 +7,5 @@ DB="./data/temperatures.db"
 if [ ! -e "$DB" ]; then
   sqlite3 "$DB" 'BEGIN; CREATE TABLE temperature (timestamp INTEGER, temperature NUMERIC); COMMIT;'
   sqlite3 "$DB" 'BEGIN; CREATE INDEX idx_temperature_timestamp ON temperature(timestamp); COMMIT;'
+  sqlite3 "$DB" 'BEGIN; PRAGMA journal_mode=WAL; COMMIT;'
 fi
